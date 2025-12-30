edc23b023c1dec92f28dd29b861471b930e42d40
Nero <137304300+NeroClaudia@users.noreply.github.com>
2025-12-30 16:37:49 +0700

    Delete PrakDasID directory

--- PrakDasID/NaikanTurunan/Ye.java
+++
@@ -1,8 +0,0 @@
-public class Ye {
-    public static void main(String[] args) {
-    
-    
-   
-    }
-
-}
--- PrakDasID/Ye/MyFirstJava07.java
+++
@@ -1,5 +0,0 @@
-public class MyFirstJava07 {
-    public static void main(String[] args) {
-        System.out.print("Aku suka animeh");
-    }
-}
